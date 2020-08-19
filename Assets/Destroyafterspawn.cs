9e1524a3f545f6a00b88e5bcd3379c941227b1d3
Twilight-Gk <e.gokulgk@gmail.com>
2020-08-19 21:24:45 +0530

    Initial commit

---
+++ Assets/Destroyafterspawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyafterspawn : MonoBehaviour
+{
+    public float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FunctionTimer.Create(() => Destroy(this.gameObject), time);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Chaser.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+//[RequireComponent(typeof(CharacterController))]
+
+public class Chaser : MonoBehaviour {
+	
+	public float speed = 20.0f;
+	public float minDist = 1f;
+	public Transform target;
+    public NavMeshAgent EnemyNavmesh;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// if no target specified, assume the player
+		if (target == null) {
+
+			if (GameObject.FindWithTag ("Player")!=null)
+			{
+				target = GameObject.FindWithTag ("Player").GetComponent<Transform>();
+			}
+		}
+        EnemyNavmesh = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (target == null)
+			return;
+
+		// face the target
+		transform.LookAt(target);
+        EnemyNavmesh.SetDestination(target.transform.position);
+		//get the distance between the chaser and the target
+		
+	}
+
+	// Set the target of the chaser
+	public void SetTarget(Transform newTarget)
+	{
+		target = newTarget;
+	}
+
+}
